ebce6ef35c33076bfd5f40174848ae41ee558c9a
João Vitor Russo <joao.00000829616@unicap.br>
2022-11-30 22:07:34 -0300

    Correção

--- src/Game/ageRating.java
+++ src/Game/ageRating.java
@@ -4,7 +4,7 @@ public enum ageRating {
     L("Adequado para todas as idades."),
     M10("Pode conter cenas de violência e linguagem imprópria moderados."),
     M12("Pode conter cenas de agressão física, insinuação de consumo de drogas e relações sexuais."),
-    M14("Pode conter cenas de Agressão física, consumo explícito de drogas e relações sexuais moderadas."),
+    M14("Pode conter cenas de agressão física, consumo explícito de drogas e relações sexuais moderadas."),
     M16("Pode conter cenas de consumo de drogas explícito, agressão física intensa e relações sexuais acentuadas"),
     M18("Pode conter cenas de indução e consumo de drogas, violência extrema, suicídio, cenas de sexo explícitas e distúrbios psicossomáticos");
 
